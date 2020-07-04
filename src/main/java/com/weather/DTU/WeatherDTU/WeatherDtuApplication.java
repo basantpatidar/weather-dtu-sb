b238fd21cc28cd5f53649d21bfabc1e219a5550e
basantpatidar <basantpatidar33@gmail.com>
2020-07-03 21:59:39 -0400

    first commit new app

---
+++ src/main/java/com/weather/DTU/WeatherDTU/WeatherDtuApplication.java
@@ -0,0 +1,13 @@
+package com.weather.DTU.WeatherDTU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDtuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDtuApplication.class, args);
+	}
+
+}
