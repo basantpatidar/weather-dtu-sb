f6f25f32ede3e0f0ed82250db545b86128ed8316
basantpatidar <basantpatidar33@gmail.com>
2020-07-05 21:51:26 -0400

    test

---
+++ src/test/java/com/weather/DTU/WeatherDTU/WeatherDtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.DTU.WeatherDTU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
