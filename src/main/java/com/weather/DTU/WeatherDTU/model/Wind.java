079fc51b9df4b686d886ea1369805a691e5c6363
basantpatidar <basantpatidar33@gmail.com>
2020-07-15 21:52:01 -0400

    wind

---
+++ src/main/java/com/weather/DTU/WeatherDTU/model/Wind.java
@@ -0,0 +1,49 @@
+package com.weather.DTU.WeatherDTU.model;
+
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+
+
+//@Entity
+public class Wind {
+//	@Id @GeneratedValue
+	private String id;
+
+	private double speed;
+	
+	private double degree;
+	
+	public Wind() {
+		
+//		this.id = UUID.randomUUID().toString();
+		
+	}
+	
+
+	public Wind(double speed, double degree) {
+		super();
+		this.speed = speed;
+		this.degree = degree;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public double getDegree() {
+		return degree;
+	}
+	public void setDegree(double degree) {
+		this.degree = degree;
+	}
+	
+	@Override
+	public String toString() {
+		return "Wind [speed=" + speed + ", degree=" + degree + "]";
+	}
+}
+
