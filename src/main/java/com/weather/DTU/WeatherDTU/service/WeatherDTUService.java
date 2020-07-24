133630de25949f0123e809a84129ebfea59da7f2
basantpatidar <basantpatidar33@gmail.com>
2020-07-23 22:15:17 -0400

    service

--- src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
+++ src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
@@ -3,8 +3,11 @@ package com.weather.DTU.WeatherDTU.service;
 import java.util.List;
 
 import com.weather.DTU.WeatherDTU.model.WeatherReading;
+import com.weather.DTU.WeatherDTU.repository.WeatherDTURepository;
 
 public class WeatherDTUService {
+	
+	WeatherDTURepository weatherDTURepository;
 
 	public List<WeatherReading> getAllReadings(){
 		
