40883cabb9ad509c0aebad5d5a974461c37dbb99
basantpatidar <basantpatidar33@gmail.com>
2020-07-30 23:16:23 -0400

    wind

--- src/main/java/com/weather/DTU/WeatherDTU/model/Wind.java
+++ src/main/java/com/weather/DTU/WeatherDTU/model/Wind.java
@@ -3,12 +3,7 @@ package com.weather.DTU.WeatherDTU.model;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.Id;
 
-
-//@Entity
 public class Wind {
 	@Id @GeneratedValue
 	private String id;
