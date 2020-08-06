c2778e9d8ac8b1a2a55aa4f85a8aba6f9c68865c
basantpatidar <basantpatidar33@gmail.com>
2020-08-05 21:23:34 -0400

    update

--- src/main/java/com/weather/DTU/WeatherDTU/controller/WeatherController.java
+++ src/main/java/com/weather/DTU/WeatherDTU/controller/WeatherController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.boot.weather.info.service.WeatherService;
 import com.weather.DTU.WeatherDTU.model.WeatherReading;
+import com.weather.DTU.WeatherDTU.service.WeatherDTUService;
 
 
 @RestController
@@ -12,6 +14,17 @@ public class WeatherController {
 
 	@Autowired
 	private WeatherReading weatherReading;
+	
+	@Autowired
+	private WeatherDTUService weatherDTUService;
+	
+//private WeatherService weatherService;
+//	
+//	@Autowired
+//	public WeatherInfoController(WeatherService weatherService) {
+//		this.weatherService = weatherService;
+//	}
+	
 	
 		@PostMapping("/getReading")
 		public WeatherReading getReading() {
