8aa390a021b83b290ce6cd14623a4e9e9c249595
basantpatidar <basantpatidar33@gmail.com>
2020-08-13 22:38:13 -0400

    wind repository

--- src/main/java/com/weather/DTU/WeatherDTU/repository/WindDTURepository.java
+++ src/main/java/com/weather/DTU/WeatherDTU/repository/WindDTURepository.java
@@ -1,10 +1,11 @@
 package com.weather.DTU.WeatherDTU.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import com.weather.DTU.WeatherDTU.model.WeatherReading;
 import com.weather.DTU.WeatherDTU.model.Wind;
 
+@Repository
 public interface WindDTURepository extends CrudRepository<Wind, String>{
 
 }
