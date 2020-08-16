3f0cf59d1c794196fb962933ed091bc85eb1e373
basantpatidar <basantpatidar33@gmail.com>
2020-08-15 22:15:36 -0400

    service

--- src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
+++ src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
@@ -1,12 +1,14 @@
 package com.weather.DTU.WeatherDTU.service;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 import com.weather.DTU.WeatherDTU.model.WeatherReading;
 import com.weather.DTU.WeatherDTU.repository.WeatherDTURepository;
 
+@Service
 public class WeatherDTUService {
 	
 	WeatherDTURepository weatherDTURepository;
