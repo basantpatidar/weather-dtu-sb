899037de3176791b1a9436b1feff65dfb044c478
basantpatidar <basantpatidar33@gmail.com>
2020-08-16 22:21:01 -0400

    repository

--- src/main/java/com/weather/DTU/WeatherDTU/repository/WeatherDTURepository.java
+++ src/main/java/com/weather/DTU/WeatherDTU/repository/WeatherDTURepository.java
@@ -1,9 +1,11 @@
 package com.weather.DTU.WeatherDTU.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.weather.DTU.WeatherDTU.model.WeatherReading;
 
+@Repository
 public interface WeatherDTURepository extends CrudRepository<WeatherReading, String>{
 
 }
