dddf490cf6b14bc406988310cd4b8374e3408d17
basantpatidar <basantpatidar33@gmail.com>
2020-09-26 21:03:00 -0400

    update

--- src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
+++ src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
@@ -4,13 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.spring.boot.app.adaptor.SignupAdaptor;
-import com.spring.boot.app.entity.UserEntity;
-import com.spring.boot.app.exception.UserNotFoundException;
-import com.spring.boot.app.model.SignupDTO;
 import com.weather.DTU.WeatherDTU.model.WeatherReading;
 import com.weather.DTU.WeatherDTU.repository.WeatherDTURepository;
 
@@ -21,12 +15,14 @@ public class WeatherDTUService {
 
 	public List<WeatherReading> getAllReadings(){
 		List<WeatherReading> list = new ArrayList<WeatherReading>();
-		List<WeatherReading> wl = weatherDTURepository.findAll();
+//		List<WeatherReading> wl 
+		return (List<WeatherReading>) weatherDTURepository.findAll();
+		
 		
 		
 //		System.out.println(weatherDTURepository.findAll());
 //		list.sort(Comparator.comparing(WeatherReading::getTimestamp));
-		return list;
+//		return list;
 		
 	}
 }
