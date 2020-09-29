dd0b5de213fe48940ef3dff225cd1aeef4224d7b
basantpatidar <basantpatidar33@gmail.com>
2020-09-28 22:00:27 -0400

    service

--- src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
+++ src/main/java/com/weather/DTU/WeatherDTU/service/WeatherDTUService.java
@@ -3,6 +3,7 @@ package com.weather.DTU.WeatherDTU.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.weather.DTU.WeatherDTU.model.WeatherReading;
@@ -10,19 +11,21 @@ import com.weather.DTU.WeatherDTU.repository.WeatherDTURepository;
 
 @Service
 public class WeatherDTUService {
-	
+	@Autowired
 	WeatherDTURepository weatherDTURepository;
 
 	public List<WeatherReading> getAllReadings(){
-		List<WeatherReading> list = new ArrayList<WeatherReading>();
+		 List<WeatherReading> list = new ArrayList<>(); 
+		 weatherDTURepository.findAll().forEach(list1->list.add(list1));
+			
 //		List<WeatherReading> wl 
-		return (List<WeatherReading>) weatherDTURepository.findAll();
+		//return (List<WeatherReading>) weatherDTURepository.findAll();
 		
 		
 		
 //		System.out.println(weatherDTURepository.findAll());
 //		list.sort(Comparator.comparing(WeatherReading::getTimestamp));
-//		return list;
+     return list;
 		
 	}
 }
