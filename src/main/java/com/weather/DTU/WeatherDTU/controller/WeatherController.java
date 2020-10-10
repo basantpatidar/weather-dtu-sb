0097f289382c3b98f39fbd117fc67008a79f7950
basantpatidar <basantpatidar33@gmail.com>
2020-10-10 17:53:00 -0400

    filter by city

--- src/main/java/com/weather/DTU/WeatherDTU/controller/WeatherController.java
+++ src/main/java/com/weather/DTU/WeatherDTU/controller/WeatherController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.weather.DTU.WeatherDTU.model.WeatherReading;
@@ -28,6 +29,13 @@ public class WeatherController {
 			List<WeatherReading> weatherReading = new ArrayList<WeatherReading>();
 			weatherReading = weatherDTUService.getBadWeather();
 			return weatherReading;
+		}
+		
+		@GetMapping("/GetCityReading")
+		public List<WeatherReading> getCityReading(@RequestParam("city") String city){
+			List<WeatherReading> list = new ArrayList<WeatherReading>();
+			list = weatherDTUService.getCityReading(city);
+			return null;
 		}
 		
 		@GetMapping("/test")
